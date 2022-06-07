fdcc115fec9fe13ed55bdd4cd792f8665d4fd428
Nikola Mitev <mitev.nkl@gmail.com>
2022-06-07 10:48:50 +0200

    a

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :lessons
+  resources :lessons do
+    resources :bookings, only: [:edit, :update]
+  end
 
   resources :questions, only: [] do
     resources :answers, only: [:create]
   end
+
+  get 'dashboard', to: 'pages#dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
