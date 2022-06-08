7ad66576b6ef8516ac19df2a993f6b505a079f83
Mael Micout <micoutmael3@gmail.com>
2022-06-08 15:52:22 +0200

    reviews done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,9 +16,18 @@ class ReviewsController < ApplicationController
     end
   end
 
-  def avg_rating
-    @review.average(:rating)
+
+  def render_stars(value)
+    output = ''
+    if (1..5).include?(value.floor)
+      value.floor.times { output += image_tag('star.gif')}
+    end
+    if value == (value.floor + 0.5) && value.to_i != 5
+      output += image_tag('half-star.gif')
+    end
+    output.html_safe
   end
+end
 
 
 
