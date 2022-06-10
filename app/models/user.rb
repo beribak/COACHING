d635fe2224a2ceb6b08fe32fe30065187c6b49c9
Nikola Mitev <mitev.nkl@gmail.com>
2022-06-10 17:25:35 +0200

    seed....3

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   has_many :reviews, through: :lessons
   has_many :bookings, dependent: :destroy
   has_many :answers, dependent: :destroy
+  has_many :messages, dependent: :destroy
 end
