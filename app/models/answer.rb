ff74941ab8bb88233aa17025d31a1f6752b12c69
Nikola Mitev <mitev.nkl@gmail.com>
2022-06-10 17:15:08 +0200

    seedSolve

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,4 @@
 class Answer < ApplicationRecord
   belongs_to :user
-  belongs_to :question, dependent: :destroy
+  belongs_to :question
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ApplicationRecord
   belongs_to :lesson
+  has_many :answers, dependent: :destroy
 end
